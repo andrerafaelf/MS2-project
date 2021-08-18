3e83037746937382d5fc3c6e263e9ddea91fd900
andrerafaelf <andrerafaelff@gmail.com>
2021-08-18 17:06:31 +0000

    Added attempts counter script and card flip sound

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ class AudioController {
         this.gameOverSound = new Audio("assets/audio/defeat.mp3");
         this.bgMusic.volume = 0.05;
         this.bgMusic.loop = true;
+        this.flipSound.volume = 0.6;
     }
 
     startMusic() {
@@ -33,18 +34,52 @@ class AudioController {
 
     gameOver() {
         this.stopMusic();
-        this.gameOverSound.play():
+        this.gameOverSound.play();
+    }
+}
+
+class MixOrMatch {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById("timer");
+        this.ticker = document.getElementById("attempts");
+        this.audioController = new AudioController();
+    }
+
+    startGame() {
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+
+    }
+
+    flipCard(card) {
+        if (this.canFlipCard(card)) {
+            this.audioController.flip();
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+        }
+    }
+
+    canFlipCard(card) {
+        return true;
+        
     }
 }
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName("landing-page"));
     let cards = Array.from(document.getElementsByClassName("card1"));
+    let game = new MixOrMatch(90, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener("click", () => {
             overlay.classList.remove("visible");
-            // game.startGame();
+            game.startGame();
             let audioController = new AudioController();
             audioController.startMusic();
         });
@@ -52,7 +87,7 @@ function ready() {
 
     cards.forEach(card => {
         card.addEventListener("click", () => {
-            // game.flipCard(card);
+            game.flipCard(card);
         });
     });
 }
