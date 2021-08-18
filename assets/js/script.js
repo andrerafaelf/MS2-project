518ecf1e8e6bad04f61ff344d7a9e5ef1a99e8f3
andrerafaelf <andrerafaelff@gmail.com>
2021-08-18 16:46:34 +0000

    Added background music to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
+class AudioController {
+    constructor() {
+        this.bgMusic = new Audio("assets/audio/bgmusic.mp3");
+        this.flipSound = new Audio("assets/audio/cardflip.wav");
+        this.matchSound =  new Audio("assets/audio/match.wav");
+        this.victorySound = new Audio("assets/audio/victory.mp3");
+        this.gameOverSound = new Audio("assets/audio/defeat.mp3");
+        this.bgMusic.volume = 0.05;
+        this.bgMusic.loop = true;
+    }
 
+    startMusic() {
+        this.bgMusic.play();
+    }
+}
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName("landing-page"));
@@ -8,6 +22,8 @@ function ready() {
         overlay.addEventListener("click", () => {
             overlay.classList.remove("visible");
             // game.startGame();
+            let audioController = new AudioController();
+            audioController.startMusic();
         });
     });
 
