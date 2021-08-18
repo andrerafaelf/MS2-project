2696300796215dd8c7df0867021f40e4d20e4c59
andrerafaelf <andrerafaelff@gmail.com>
2021-08-18 16:26:31 +0000

    Fixed bug regarding script source in index.html and added event listener to landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@ if(document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", ready());
 } else {
     ready();
-};
+}
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName("landing-page"));
     let cards = Array.from(document.getElementsByClassName("card1"));
-    
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener("click", () => {
+            overlay.classList.remove("visible");
+        });
+    });
 }
